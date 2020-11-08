7269ab71b08abaf834954c993e1465c6d3ea1ef8
sunkyu Kim <yyh2337@naver.com>
2020-11-09 03:45:48 +0900

    장면분할 API적용

--- PracticeUseAPI/Example_1.py
+++ PracticeUseAPI/Example_1.py
@@ -1,14 +1,14 @@
-'''홈페이지 구현예제_1(20201106) by Python3#'''
- # -*- coding:utf-8 -*-
 import urllib3
+'''홈페이지 구현예제_1(20201106) by Python3#'''
 import json
 import os
+import time
 
 openApiURL = "http://aiopen.etri.re.kr:8000/VideoParse" #API호출
 accessKey = "16f35e3e-b273-4953-9366-4c7d68128530" #API접근 Key
-videoFilePath = "/Users/sunkyu/Documents/ETRIOpenApiContest/PracticeUseAPI/UsingSleight.mp4" #파일 주소
+videoFilePath = "/Users/sunkyu/Documents/ETRIOpenApiContest/PracticeUseAPI/videoplayback.mp4" #파일 주소
 
-file = open(videoFilePath,'rb') #파일 열기 (이진파일 읽기 권한부여)
+file = open(videoFilePath,'rb')#파일 열기 (이진파일 읽기 권한부여)
 fileContent = file.read()
 file.close()
 
@@ -23,12 +23,36 @@ response = http.request(
   "POST",
   openApiURL,
   fields={
-    'json':json.dumps(requestJson),
-    'uploadfile':(os.path.basename(file.name), fileContent)
+    'json': json.dumps(requestJson),
+    'uploadfile': (os.path.basename(file.name), fileContent)
   }
 )
 
 print("[responseCode]" + str(response.status))
-print("[responBody")
+print("[responseBody]")
 print(response.data)
 
+time.sleep(60)
+
+URL = openApiURL + '/status'
+file_id = json.loads(response.data.decode())["return_object"]["file_id"]
+
+requestJson1 = {
+  "access_key": accessKey,
+  "argument":{"file_id": file_id}
+}
+
+http1 = urllib3.PoolManager()
+
+response1 = http1.request(
+  "POST",
+  URL,
+  fields={
+    'json': json.dumps(requestJson1),
+    'uploadfile': (os.path.basename(file.name), fileContent)
+  }
+)
+
+print("[responseCode]" + str(response1.status))
+print("[responseBody]")
+print(response1.data)
