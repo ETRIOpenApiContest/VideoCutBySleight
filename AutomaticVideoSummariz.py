74037f1db28caa8765e756b82917286a25fb0e66
sunkyu Kim <yyh2337@naver.com>
2020-11-15 23:48:53 +0900

    최종 결과물

--- AutomaticVideoSummariz.py
+++ AutomaticVideoSummariz.py
@@ -8,9 +8,10 @@ import moviepy.editor as mp
 from pydub import AudioSegment
 
 openApiURL = "http://aiopen.etri.re.kr:8000/VideoParse" #API호출
-accessKey = "16f35e3e-b273-4953-9366-4c7d68128530" #API접근 Key
-path = "/Users/sunkyu/Documents/ETRIOpenApiContest/PracticeUseAPI/videoplayback.mp4" #파일 주소
-video = '/videoplayback.mp4'
+accessKey = "bd0d5ad2-71f3-4398-b24c-6eaddc462a05" #API접근 Key
+path = r'C:\Users\skanj\Desktop\video'
+video = '\movie4.mp4'
+
 videoFilePath = path + video
 
 file = open(videoFilePath,'rb')#파일 열기 (이진파일 읽기 권한부여)
@@ -38,7 +39,7 @@ print("[responseBody]")
 print(response.data) #파일 ID 출력
 # json파일 형식으로 다운로드
 with open("fileId.json", "w") as json_file:
-  json.dump(response.data.decode('utf-8'), json_file)
+  json.dump(response.data.decode('utf8'), json_file)
 
 time.sleep(30) #통신하는 시간 딜레이 설정
 
@@ -66,11 +67,11 @@ print("[responseBody]")
 print(response1.data) #장면분할 결과 출력
 # json파일 형식으로 다운로드
 with open("divideResult.json", "w") as json_file:
-  json.dump(response1.data.decode('utf-8'), json_file)
+  json.dump(response1.data.decode('utf8'), json_file)
 
 # audio path, language, API
 languageCode = "korean"
-audio = "/movie4.wav"
+audio = "\movie4.wav"
 audioFilePath = path + audio
 openApiURL =  "http://aiopen.etri.re.kr:8000/WiseASR/Recognition"
 
@@ -81,42 +82,56 @@ clip.audio.write_audiofile(audioFilePath)
 # audio
 sound = AudioSegment.from_file(audioFilePath)
 
-# 일단 앞의 5초 나누기 
-split_point = 5000
-splits = sound[:split_point]
+# ddd
+moviedict = json.loads(response1.data.decode('utf-8'))['return_object']
 
-# create a new split audio file
-split_audio_name = "/s1.wav"
-split_audio_path = path + split_audio_name
+#dd
+split_points = []
+splits = []
+for i in range(1,len(moviedict['result'][0]['time'])):
+    j = moviedict['result'][0]['time'][i] * len(sound)
+    k = moviedict['result'][0]['time'][i-1] * len(sound)
+    split_point = [k,j]
+    split_points.append(split_point)
 
-# audio split
-splits.export(split_audio_path, format="wav")
+# dd
+for i in range(len(split_points)):
+    splits.append(sound[split_points[i][0]:split_points[i][1]])
+    
+# dd
+for i in range(len(splits)):
+    print( i, "번째 소리입니다.")
+    split_audio_name = "\s" + str(i) + ".wav"
+    split_audio_path = path + split_audio_name
+    
+    # audio split
+    splits[i].export(split_audio_path, format="wav")
 
-# 음성인식 API 시작 앞에 5초만 나눈 오디오로 함
-file = open(split_audio_path, "rb")
-audioContents = base64.b64encode(file.read()).decode("utf8")
-file.close()
+    # 음성인식 API 시작 앞에 5초만 나눈 오디오로 함
+    file = open(split_audio_path, "rb")
+    audioContents = base64.b64encode(file.read()).decode("utf8")
+    file.close()
 
-requestJson = {
-  "acces_key": accessKey,
-  "argument": {
-    "language_code": languageCode,
-    "audio": audioContents
-  }
-}
+    requestJson = {
+      "acces_key": accessKey,
+      "argument": {
+        "language_code": languageCode,
+        "audio": audioContents
+      }
+    }
 
-http = urllib3.PoolManager()
-response = http.request(
-  "POST",
-  openApiURL,
-  headers={"Content-Type": "application/json; charset=UTF-8"},
-  body = json.dumps(requestJson)
-)
+    http = urllib3.PoolManager()
+    response = http.request(
+      "POST",
+      openApiURL,
+      headers={"Content-Type": "application/json; charset=UTF-8"},
+      body = json.dumps(requestJson)
+    )
 
-print("[responseCode] " + str(response.status))
-print("[responBody")
-print(str(response.data, "utf-8")) 
-# json파일 형식으로 다운로드
-with open("audio_file.json", "w") as json_file: 
-  json.dump(response.data.decode('utf-8'), json_file) 
-
+    print("[responseCode] " + str(response.status))
+    print("[responBody]")
+    print(str(response.data, "utf-8")) 
+    # json파일 형식으로 다운로드
+    audio_file_json = "audio_file" + str(i) + ".json"
+    with open(audio_file_json, "w") as json_file: 
+      json.dump(response.data.decode('utf8'), json_file) 
